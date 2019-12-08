a20395b7b53c652ecb23298cf5c4f991afcf1818
Paco Juan <pacojuanquiros@gmail.com>
2019-12-08 02:32:34 +0100

    Spawning balls

--- Assets/Scripts/CartBehaviour.cs
+++ Assets/Scripts/CartBehaviour.cs
@@ -6,24 +6,47 @@ using UnityEngine;
 public class CartBehaviour : MonoBehaviour
 {
 
+    // Next cart we have to shoot from
+    public static int NextCartId = 0;
+
     public int CartId;
     
+    private int _ballCount;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _ballCount = 5;
     }
 
 
     public void OnMouseDown()
     {
+        if (_ballCount <= 0)
+        {
+            Debug.LogWarning("The cart has no balls left.");
+            return;
+        }
+        if (NextCartId != this.CartId)
+        {
+            Debug.LogWarning("Cannot pick ball yet.");
+            return;
+        }
+        
+        PlayerBehaviour player = FindObjectOfType<PlayerBehaviour>();
+        if (!player.SpawnBall())
+        {
+            Debug.LogWarning("The player cannot spawn any ball.");
+            return;
+        }
+
+        Transform ballTransform = transform.GetChild(1).GetChild(_ballCount - 1);
+        ballTransform.gameObject.SetActive(false);
+
+        _ballCount--;
+        if (_ballCount <= 0)
+            NextCartId++;
+        
         Debug.Log("Ball picked!");
     }
 }
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public Transform SpawnAnchor;
+
+    public BallBehaviour BallPrefab;
+
+
+    private BallBehaviour _current;
+
+
+    public void Update()
+    {
+        if (_current == null)
+            return;
+        
+        _current.transform.localEulerAngles = new Vector3(
+                -this.transform.localEulerAngles.x,
+                0,
+                0
+            );
+    }
+    
+    
+    public bool SpawnBall()
+    {
+        if (_current != null)
+            return false;
+
+
+        BallBehaviour ball = Instantiate(BallPrefab, SpawnAnchor);
+
+
+        return true;
+    }
+
+}
